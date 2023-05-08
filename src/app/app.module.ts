dd6a57bd13aa1fe9553702f528b8a595208400cc
Petr Vocelka <petr.vocelka@alza.cz>
2023-05-08 22:31:22 +0100

    Enable DEV build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './ui/input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,30 @@ import { createApplication } from '@angular/platform-browser';
 import { createCustomElement } from '@angular/elements';
 import { InputComponent } from './app/ui/input/input.component';
 
-(async () => {
-  const app = await createApplication({
-    providers: [],
-  });
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 
-  const inputElement = createCustomElement(InputComponent, {
-    injector: app.injector,
-  });
-  customElements.define('wc-input', inputElement);
-})();
+if (environment.production) {
+  enableProdMode();
+
+  (async () => {
+    const app = await createApplication({
+      providers: [],
+    });
+
+    const inputElement = createCustomElement(InputComponent, {
+      injector: app.injector,
+    });
+    customElements.define('wc-input', inputElement);
+  })();
+
+}
+
+
+if (!environment.production) {
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+
+}
